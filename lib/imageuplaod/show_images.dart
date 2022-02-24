1614c071d785676004a20cfb0fab3bc473aa68fb
samratmiddha <98886131+samratmiddha@users.noreply.github.com>
2022-02-24 23:27:22 +0530

    saving code

---
+++ lib/imageuplaod/show_images.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ShowUploads extends StatefulWidget {
+  String? userId;
+  ShowUploads({Key? key}) : super(key: key);
+
+  @override
+  _ShowUploadsState createState() => _ShowUploadsState();
+}
+
+class _ShowUploadsState extends State<ShowUploads> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Your Images")),
+        body: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection("users")
+                .doc(widget.userId)
+                .collection("images")
+                .snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return (const Center(child: Text("No image uploaded")));
+              } else {
+                return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      String url = snapshot.data!.docs[index]['downloadURL'];
+                      return Image.network(
+                        url,
+                        height: 300,
+                        fit: BoxFit.cover,
+                      );
+                    });
+              }
+            }));
+  }
+}
